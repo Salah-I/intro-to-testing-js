56ec294c69d77bf69ebe201e18f6efb54034a6f0
Salah Ismail <salah.o.ismail01@gmail.com>
2020-10-01 16:45:57 -0500

    my first test

--- code.js
+++ code.js
@@ -3,3 +3,48 @@ function hello() {
     return "Hello, World!"
         ;
 }
+const helloWorld = function (){
+    return "Hello, World";
+};
+
+function sayhello(name){
+    return "Hello" + name; || "Hello" + Pat || "Hello" + Alex || "Hello" + Jane
+}
+
+
+//// helloWorld function
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+//
+// function sayHello(name){
+//     if(typeof name == 'undefined' || typeof name == 'boolean'){
+//         return "Hello, World!";
+//     }
+//     return "Hello, " + name + "!";
+// }
+//
+// function isFive(x){
+//     return x == 5;
+//
+// }
+//
+// function isEven(x){
+//     return x % 2 === 0 && typeof x !== 'boolean';
+// }
+//
+// function isVowel(letter){
+//     switch(letter){
+//         case "a": case "A": case "e": case "E": case "i": case "I": case "o": case "O": case "u": case "U":
+//             return true;
+//     }
+//     return false;
+// }
+//
+// function add(a,b){
+//     if(isNaN(a) || isNaN(b)){
+//         return "NaN";
+//     }else {
+//         return Number(a) + Number(b);
+//     }
+// }
--- tests.js
+++ tests.js
@@ -16,23 +16,176 @@ describe('hello', function() {
 
 });
 
-describe('sayHello', function() {
-    it('should be a defined function', function() {
-        expect(typeof sayHello).toBe('function');
-    });
-    });
-
-
-}
-describe('hello', function() {
+describe('sayHello', function(){
     it('should be a defined function', function () {
         expect(typeof hello).toBe('function');
     });
+});
+
+
+
+describe('sayHello', function() {
+    it('should be a defined function', function () {
+        expect(typeof sayhello).toBe('function');
+    });
     it('should return a string Hello Jane', function () {
-        expect(typeof hello()).toBe("Hello, Jane");
+        expect(typeof sayhello()).toBe("Hello, Jane");
     });
     it('should return the string "Hello, Alex!" when executed', function () {
-        expect(hello()).toBe("Hello, Alex");
+        expect(typeof sayhello()).toBe("Hello, Alex");
     })
-})
+    it('should return string "Hello, Pat" when exucted', function () {
+        expect(typeof sayhello()).toBe("Hello, Pat")
 
+        });
+    })
+
+//// Unit tests for the helloWorld function
+// describe('helloWorld', function() {
+//     it('should be a defined function', function() {
+//       expect(typeof helloWorld).toBe('function');
+//     });
+//     it('should return a string when called', function() {
+//       expect(typeof helloWorld()).toBe("string");
+//     });
+//     it('should return the string "Hello, World!" when executed', function() {
+//       expect(helloWorld()).toBe("Hello, World!");
+//     });
+//     it("should never return 'undefined' when called", function() {
+//       expect(helloWorld()).not.toBe(undefined);
+//     });
+// });
+//
+// describe('sayHello', function() {
+//     it('should be a defined function', function() {
+//         expect(typeof sayHello).toBe('function');
+//     });
+//     it('should return a string when called', function() {
+//         expect(typeof sayHello()).toBe('string');
+//     });
+//     it('should return the string "Hello, Jane!" when executed', function() {
+//         expect(sayHello("Jane")).toBe("Hello, Jane!");
+//     });
+//     it('should return the string "Hello, Alex!" when executed', function() {
+//         expect(sayHello("Alex")).toBe("Hello, Alex!");
+//     });
+//     it('should return the string "Hello, Pat!" when executed', function() {
+//         expect(sayHello("Pat")).toBe("Hello, Pat!");
+//     });
+//     it('should return the string "Hello, World!"', function() {
+//         expect(sayHello()).toBe("Hello, World!");
+//     });
+//     it('should return the string "Hello, World!', function() {
+//         expect(sayHello(true)).toBe("Hello, World!");
+//     });
+//     it('should return the string "Hello, World!', function() {
+//         expect(sayHello(false)).toBe("Hello, World!");
+//     });
+// });
+//
+// describe('isFive', function() {
+//     it('should be a defined function', function() {
+//         expect(typeof isFive).toBe('function');
+//     });
+//     it('should return a boolean no matter what the input', function() {
+//         expect(typeof isFive()).toBe('boolean');
+//     });
+//     it('should return "true" when passed 5', function() {
+//         expect(isFive(5)).toBe(true);
+//     });
+//     it('should return "true" when passed "5"', function() {
+//         expect(isFive("5")).toBe(true);
+//     });
+// });
+//
+// describe('isEven', function() {
+//     it('should be a defined function', function() {
+//         expect(typeof isEven).toBe('function');
+//     });
+//     it('should return a boolean no matter what the input', function() {
+//         expect(typeof isEven()).toBe('boolean')
+//     });
+//     it('should return "true" when passed 2', function() {
+//         expect(isEven(2)).toBe(true);
+//     });
+//     it('should return "true" when passed -4', function() {
+//         expect(isEven(-4)).toBe(true);
+//     });
+//     it('should return "true" when passed "8"', function() {
+//         expect(isEven(8)).toBe(true);
+//     });
+//     it('should return "false" when passed 3', function() {
+//         expect(isEven(3)).toBe(false);
+//     });
+//     it('should return "false" when passed "banana"', function() {
+//         expect(isEven("banana")).toBe(false);
+//     });
+//     it('should return "false" when passed Infinity', function() {
+//         expect(isEven(Infinity)).toBe(false);
+//     });
+//     it('should return "false" when passed a boolean', function() {
+//         expect(isEven(true)).toBe(false);
+//         expect(isEven(false)).toBe(false);
+//     });
+//     it('should return "false" when called without an argument', function() {
+//         expect(isEven()).toBe(false);
+//     });
+// });
+//
+// describe('isVowel', function() {
+//     it('should be a defined function', function() {
+//         expect(typeof isVowel).toBe('function');
+//     });
+//     it('should return a boolean no matter the input', function() {
+//         expect(typeof isVowel()).toBe('boolean');
+//     });
+//     it('should return "true" when passed a vowel', function() {
+//         expect(isVowel("a")).toBe(true);
+//     });
+//     it('should return "true" when passed a vowel', function() {
+//         expect(isVowel("A")).toBe(true);
+//     });
+//     it('should return "false" when passed a consonant', function() {
+//         expect(isVowel("y")).toBe(false);
+//     });
+//     it('should return "false" when passed a number', function() {
+//         expect(isVowel(4)).toBe(false);
+//     });
+//     it('should return "false" when passed a boolean', function() {
+//         expect(isVowel(true)).toBe(false);
+//         expect(isVowel(false)).toBe(false);
+//     });
+//     it('should return "false" when passed a string', function() {
+//         expect(isVowel("banana")).toBe(false);
+//     });
+//     it('should return "false" when called without an argument', function() {
+//         expect(isVowel()).toBe(false);
+//     });
+// });
+//
+// describe('add', function() {
+//     it('should be a defined function', function() {
+//         expect(typeof add).toBe('function');
+//     });
+//     it('should return 5 when passed 2 and 3', function() {
+//         expect(add(2,3)).toBe(5);
+//     });
+//     it('should return -12 when passed -3 and -9', function() {
+//         expect(add(-3,-9)).toBe(-12);
+//     });
+//     it('should return 5 when passed 2 and 3', function() {
+//         expect(add(2,3)).toBe(5);
+//     });
+//     it('should return 11 when passed "5" and 6', function() {
+//         expect(add("5",6)).toBe(11);
+//     });
+//     it('should return NaN when passed "banana" and "split"', function() {
+//         expect(add("banana","split")).toBe("NaN");
+//     });
+//     it('should return NaN when passed "2" and "apples"', function() {
+//         expect(add("2","apples")).toBe("NaN");
+//     });
+//     it('should return NaN when called without arguments', function() {
+//         expect(add()).toBe("NaN");
+//     });
+// });
